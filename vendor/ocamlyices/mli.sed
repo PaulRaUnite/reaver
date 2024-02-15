6ed6529b50c2e3c5aa57a67da520c15bf1796fa3
Pavlo Tokariev <eclipsepaulra@gmail.com>
2024-02-15 21:49:51 +0000

    ocamlyices: now in separate repository, also built with dune.

--- vendor/ocamlyices/mli.sed
+++
@@ -1,4 +0,0 @@
-/^and var_decl_iterator$$/ d
-/^and scalar_\(int\|name\)\b/ d
-s/scalar_name/string/g
-s/scalar_int/int/g
--- vendor/ocamlyices/test/util.ml
+++
@@ -1,9 +0,0 @@
-open OUnit2
-
-let bracket_mk_context test_ctxt =
-  bracket
-    (fun _ -> Yices.mk_context ())
-    (fun ctx _ -> Yices.del_context ctx) test_ctxt
-
-let simple_test f test_ctxt =
-  let ctx = bracket_mk_context test_ctxt in ignore (f ctx)
